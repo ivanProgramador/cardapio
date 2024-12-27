38dc2019d54d7c6ed4e98e8e9148e9a2292a3826
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-12-27 16:03:45 -0300

    interface pronta

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,css}"],
   theme: {
+    fontFamily:{
+      'sans':['Roboto','sans-serif']
+    },
     extend: {
+
        backgroundImage:{
          "home":"url('/assets/bg.png')"
        }
